b8950c20f0d98eb2e8faf99a244e889d237a0e73
Bzemke <zman2185@hotmail.com>
2016-11-03 18:57:05 -0500

    Following along in class

--- Exercise2/scripts/main.js
+++ Exercise2/scripts/main.js
@@ -1,6 +1,27 @@
 /*******Part 1***********/
 //Place all of your part 1 code here
+var x = 120;
+var y = 90;
 
+var z = x > y;
+
+console.log('z' + " " + z)
+console.log(z)
+
+////////////////
+console.log("start math")
+x = 10;
+x += 25;
+console.log(x)
+x += 35;
+console.log(x)
+x = x * 2
+console.log(x)
+console.log("end math")
+
+////////////////
+
+var one = 1;
 
 
 
@@ -16,7 +37,3 @@ var foo = "This is a String";
 /*******Part 4***********/
 var fName = "John"
 var lName = "Doe"
-
-
-
-
