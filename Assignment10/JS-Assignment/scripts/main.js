cf8803e3392d5ee75931c2888c460276b082b124
Bzemke <zman2185@hotmail.com>
2016-11-05 13:36:01 -0500

    Started doing bonus work

--- Assignment10/JS-Assignment/scripts/main.js
+++ Assignment10/JS-Assignment/scripts/main.js
@@ -14,19 +14,29 @@ function update() {
 
     if(firstName != "" && lastName != ""){
       document.getElementById('p1_valid').innerHTML = true;
+      document.getElementById('p1_fullNameLength').innerHTML = fullName.length;
+      document.getElementById('p1_fullNameLength2').innerHTML = fullName.length - 1;
     } else {
       document.getElementById('p1_valid').innerHTML = false;
-    }
+      document.getElementById('p1_fullName').innerHTML = "Invalid";
+      document.getElementById('p1_fullNameLength').innerHTML = "Invalid";
+      document.getElementById('p1_fullNameLength2').innerHTML = "Invalid";
+     }
 
-    document.getElementById('p1_fullNameLength').innerHTML = fullName.length;
-    document.getElementById('p1_fullNameLength2').innerHTML = fullName.length - 1;
 
     // Part 2
     var emailAddress = getValue('emailAddress');
     var isValidEmail = emailAddress.indexOf('@')
     console.log(emailAddress);
-    document.getElementById('p2_email').innerHTML = emailAddress;
-    document.getElementById('p2_emailLength').innerHTML = emailAddress.length;
+    if(emailAddress != ""){
+      document.getElementById('p2_email').innerHTML = emailAddress;
+      document.getElementById('p2_emailLength').innerHTML = emailAddress.length;
+    } else {
+      document.getElementById('p2_email').innerHTML = "Invalid";
+      document.getElementById('p2_emailLength').innerHTML = "Invalid";
+    }
+
+
 
     if(isValidEmail > -1){
       document.getElementById('p2_valid').innerHTML = true;
@@ -38,12 +48,15 @@ function update() {
     // Part 3
     var randomNumber = getValue('randomNumber');
     console.log(randomNumber);
-    document.getElementById('p3_number').innerHTML = randomNumber;
-
-    if(randomNumber >= 0 || randomNumber <= 0){
+    parseInt(randomNumber);
+    console.log(randomNumber);
+  ///    document.getElementById('p3_valid').innerHTML = "Invalid";
+    if (randomNumber >= 0 || randomNumber < 0){
+      document.getElementById('p3_number').innerHTML = randomNumber;
       document.getElementById('p3_valid').innerHTML = true;
     } else {
       document.getElementById('p3_valid').innerHTML = false;
+          document.getElementById('p3_number').innerHTML = "Invalid";
     }
 
 
@@ -53,7 +66,10 @@ function update() {
     var arrayInput = getValue('arrayValue');
     console.log(arrayInput);
     console.log(array);
-    array.push(arrayInput);
+    if (arrayInput != "") {
+        array.push(arrayInput);
+    }
+
     console.log(array);
     document.getElementById('p4_arrayLength').innerHTML = array.length;
     document.getElementById('p4_display').innerHTML = array;
