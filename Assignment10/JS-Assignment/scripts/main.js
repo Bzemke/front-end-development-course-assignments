2c5a85fdb0773389b8ed35c9583d66cdfd08355b
Bzemke <zman2185@hotmail.com>
2016-11-05 12:37:54 -0500

    Completed javascript portion
    Typo corrections in readme, completed Javascript portion of the
    assignment.

--- Assignment10/JS-Assignment/scripts/main.js
+++ Assignment10/JS-Assignment/scripts/main.js
@@ -1,3 +1,5 @@
+    var array = [];
+
 function update() {
     //////////////////////Your Code Starts here
 
@@ -5,20 +7,44 @@ function update() {
     ///Part 1
     var firstName = getValue('firstName');
     var lastName = getValue('lastName');
+    var fullName = firstName + " " + lastName;
     console.log(firstName);
     console.log(lastName);
+    document.getElementById('p1_fullName').innerHTML = fullName;
 
+    if(firstName != "" && lastName != ""){
+      document.getElementById('p1_valid').innerHTML = true;
+    } else {
+      document.getElementById('p1_valid').innerHTML = false;
+    }
 
-
+    document.getElementById('p1_fullNameLength').innerHTML = fullName.length;
+    document.getElementById('p1_fullNameLength2').innerHTML = fullName.length - 1;
 
     // Part 2
     var emailAddress = getValue('emailAddress');
+    var isValidEmail = emailAddress.indexOf('@')
     console.log(emailAddress);
+    document.getElementById('p2_email').innerHTML = emailAddress;
+    document.getElementById('p2_emailLength').innerHTML = emailAddress.length;
+
+    if(isValidEmail > -1){
+      document.getElementById('p2_valid').innerHTML = true;
+    } else {
+      document.getElementById('p2_valid').innerHTML = false;
+    }
 
 
     // Part 3
     var randomNumber = getValue('randomNumber');
     console.log(randomNumber);
+    document.getElementById('p3_number').innerHTML = randomNumber;
+
+    if(randomNumber >= 0 || randomNumber <= 0){
+      document.getElementById('p3_valid').innerHTML = true;
+    } else {
+      document.getElementById('p3_valid').innerHTML = false;
+    }
 
 
 
@@ -26,6 +52,11 @@ function update() {
     // Part 4
     var arrayInput = getValue('arrayValue');
     console.log(arrayInput);
+    console.log(array);
+    array.push(arrayInput);
+    console.log(array);
+    document.getElementById('p4_arrayLength').innerHTML = array.length;
+    document.getElementById('p4_display').innerHTML = array;
 
 
 
@@ -33,11 +64,7 @@ function update() {
 
 
 
-
-
-
-
-    ////////////////////////Youre code ends here.
+    ////////////////////////Your code ends here.
 
 }
 
