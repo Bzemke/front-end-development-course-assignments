ba3ff2ab96052d3be5687983f7665a3acac18aa8
Bzemke <zman2185@hotmail.com>
2016-11-29 19:12:16 -0600

    Completed assignment 15
    All sections working as intended.

--- Assignment15/scripts/main.js
+++ Assignment15/scripts/main.js
@@ -2,9 +2,26 @@ $(document).ready(function(){
 
      ///// Code goes here
 
+$('#btnOne').on('click', function(){
+    alert('You clicked the first button!');
+});
 
+$('#btnTwo').on('click', function(){
+  $('.update-html').html('Hello World')
+});
 
+$('#btnThree').on('click', function(){
+  $('#text-update').val('Hello World')
+});
 
+$('#btnCopyHtml').on('click', function(){
+  var copiedHtml = $('.copy-html').html()
+  $('.paste-html').html(copiedHtml)
+});
 
+$('#btnCopyValues').on('click', function(){
+  var copiedValue = $('#copy-text').val()
+  $('#paste-text').val(copiedValue)
+});
 
 });
