6ad497a41286cd363db7244aa480644d252aee74
Bzemke <zman2185@hotmail.com>
2016-12-08 18:03:57 -0600

    Beginning of code changes

--- Assignment17/scripts/main.js
+++ Assignment17/scripts/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 
+$('.error').hide();
+$('.input-group').filter(':even').addClass('even');
 
+// var inputs = $('input')
+// var evenInputs = inputs.filter(':even');
+
+  // evenInputs.addClass('even');
+  // console.log(evenInputs)
+
+  $('#firstName').on('blur', function(){
+    var fName = $(this);
+    var msg = fName.closest('.input-group').find('.requiredMsg');
+
+	if(fName.val() === ''){
+    msg.fadeIn();
+  }else{
+    msg.fadeOut();
+  }
+  });
 
 });
