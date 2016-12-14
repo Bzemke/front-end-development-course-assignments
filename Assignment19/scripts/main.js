b54c47340ce673e8e4b5ba66b01f3182f69a06e0
Bzemke <zman2185@hotmail.com>
2016-12-13 19:48:35 -0600

    Completed assingment.  Indexing is off by one when compared to the example provided ranging from 1-10 then 50-1 instead of 0-9 and 49-0

--- Assignment19/scripts/main.js
+++ Assignment19/scripts/main.js
@@ -7,5 +7,38 @@ $(document).ready(function () {
     //
     var prependUrl = 'http://jsonplaceholder.typicode.com/photos?albumId=1';
 
+    var appendList = document.getElementById('#append')
+    // console.log(appendList)
 
-});
+    $.ajax({
+      url: appendUrl,
+      method: 'get',
+      success: function(response){
+        $('.cl').html(response);
+        var appendData = response;
+        var ul = $('#append');
+        for(i=0; i < response.length; i++){
+          var post = response[i];
+          var li = '<li>Index: '+ (i+1) + 'Title: ' + post.title + '</li>';
+          ul.append(li);
+          // console.log(post)
+        }
+      }
+      })
+      // failure: function(){
+        // console.log('failure')
+      })
+
+    // }
+    var prependUrl = 'http://jsonplaceholder.typicode.com/photos?albumId=1';
+    $.get(prependUrl, function(response){
+      console.log(response);
+      var ul = $('#prepend');
+
+
+      for(i=0; i < response.length; i++){
+        var postTwo = response[i];
+        var li = '<li>Index: '+ (i+1) + 'Title: ' + postTwo.title + '</li>';
+        ul.prepend(li);
+      }
+    })
